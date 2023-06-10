f3103327810ab92f0f17385fe3378eb4b584b4a3
Arka <arkaprava.kgp@gmail.com>
2023-06-10 21:59:27 +0530

    first commit made by arka

---
+++ prog1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class Room
+{
+int l,b,h;
+
+void getc(int len,int bre,int hei)
+
+{
+l=len;
+b=bre;
+h=hei;
+}
+int volume()
+{
+int vol;
+vol=l*b*h;
+return vol;
+}
+}
+class RoomDemo extends Room
+{
+void display()
+{
+System.out.println("the volume is : "+volume());
+}
+}
+class Main{
+public static void main(String args[])
+{
+RoomDemo ob=new RoomDemo();
+int a,b,c;
+System.out.println("enter the length breadh and height respectively");
+Scanner sc= new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+c=sc.nextInt();
+ob.getc(a,b,c);
+ob.display();
+}
+}
